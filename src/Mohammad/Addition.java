40c430f435d422bf403ec8a48e5d2a677cef4a30
MohmmadKaif <Sayyadkaif4098@gmail.com>
2021-06-08 15:18:56 +0530

    In this part of course we have learnt houw to print the outpu ion web page using PrintWriter object and we also saw how to create PrintWriter Object......

--- src/Mohammad/Addition.java
+++ src/Mohammad/Addition.java
@@ -1,4 +1,7 @@
 package Mohammad;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,10 +14,19 @@ import javax.servlet.http.HttpServletResponse;
 */
 public class Addition extends HttpServlet      
 {
- public void service(HttpServletRequest req,HttpServletResponse res) 
+ public void service(HttpServletRequest req,HttpServletResponse res) throws IOException 
  {
    int i=Integer.parseInt(req.getParameter("num1"));
    int j= Integer.parseInt( req.getParameter("num2"));
+   
+   /*
+    *  Now to print our answer on output screen what we do is create an object of class PrintWriter.
+    *  This is done with the help getWriter method which is called using response object...
+    *  Below is the demonstration for this
+    */
+   
+   PrintWriter out=res.getWriter();
+   out.println( i+j );
    System.out.println(i+j);
  }
 	
