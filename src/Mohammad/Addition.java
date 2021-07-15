02d2ceb7d33da5088e4d36ccc43e895ee102f017
MohammadKaif <sayyadkaif4098@gmail.com>
2021-07-15 15:05:17 +0530

    Concept of ServletContext

--- src/Mohammad/Addition.java
+++ src/Mohammad/Addition.java
@@ -1,8 +1,9 @@
 package Mohammad;
 import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,21 +18,15 @@ public class Addition extends HttpServlet
 {
  public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException 
  {
-   long i=Integer.parseInt(req.getParameter("num1"));
-   long j= Integer.parseInt( req.getParameter("num2"));
-   
-   long r= i+j;
-   
-   /*
-    * Here in this example we will be demonstrating the concept of Cookie...
-    * Whenever a Servlets wants to call a Servlet we can use Cookie to send the data to client and then client will make the request to the   another aervlet..*/
-   
-   Cookie cookie = new Cookie( "res", r + "" );//Here we are making the Cookie Object...
-                                               //The Constructor of Cookie class takes two arguments one being the data name and other being the data.You can give any name to your data.....
-   
-   res.addCookie(cookie);//Adding cookie to the response object so that we can send it to client...
-   
-   res.sendRedirect("sq");//Here we are sending a redirect request to square servlet...
+     // This is a demonstration for ServletContext
+	 
+	   
+	 PrintWriter out= res.getWriter();
+	 //Now to get the data which is set in web.xml we will use object of ServletContext...
+	 ServletContext cnt= getServletContext();
+     out.println(cnt.getInitParameter("login-id")/*Here we are getting the parameter specified in web.xml*/);//And now we are printing the parmater...
+	 //Note: While ising ServletContext the context specified in web.xml file will be avialable for each and every servlet...
+	 
   }
 	
 }
--- src/Mohammad/Squre.java
+++ src/Mohammad/Squre.java
@@ -12,29 +12,6 @@ public class Squre extends HttpServlet
 {
 public void doGet( HttpServletRequest req, HttpServletResponse res) throws IOException
 {
-    //Here First of all we receive the cookie..
-	
-	long RD=-1000;
-	Cookie cookie[] = req.getCookies();//This method will return all the cookies which we got from the servlet which called this servlet.. 
-	
-	for(Cookie c: cookie )
-	{
-		if(c.getName().equals("res"))
-		{
-			RD=(long)Integer.parseInt(c.getValue());
-		    break;
-		}
-		
-	}
-	
-	
-	System.out.println( RD );
-	
-    
-	PrintWriter out= res.getWriter();//Creating the object of PrintWriter....
-    out.println( RD );//Printing the value of RD on webpage...
-    long ans= RD*RD;//Sqauring the value..
-    out.println(ans);//Printing the value of RD on webpage....
 	
 }
 
