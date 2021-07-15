51f397ceeb3ff9823147335465665447cb42db16
MohammadKaif <sayyadkaif4098@gmail.com>
2021-07-15 17:28:21 +0530

    Concept of ServletCofig

--- src/Mohammad/Addition.java
+++ src/Mohammad/Addition.java
@@ -2,7 +2,7 @@ package Mohammad;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletContext;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,12 +18,12 @@ public class Addition extends HttpServlet
 {
  public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException 
  {
-     // This is a demonstration for ServletContext
+     // This is a demonstration for ServletConfig
 	 
 	   
 	 PrintWriter out= res.getWriter();
-	 //Now to get the data which is set in web.xml we will use object of ServletContext...
-	 ServletContext cnt= getServletContext();
+	 //Now to get the data which is set in web.xml we will use object of ServletConfig...
+	 ServletConfig cnt= getServletConfig();
      out.println(cnt.getInitParameter("login-id")/*Here we are getting the parameter specified in web.xml*/);//And now we are printing the parmater...
 	 //Note: While ising ServletContext the context specified in web.xml file will be avialable for each and every servlet...
 	 
