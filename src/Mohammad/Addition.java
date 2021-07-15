f89b77fe4085fae04527e27aef010fc2a3e3dba1
MohammadKaif <sayyadkaif4098@gmail.com>
2021-07-15 18:01:28 +0530

    Concept of Servlet Annotations

--- src/Mohammad/Addition.java
+++ src/Mohammad/Addition.java
@@ -2,8 +2,9 @@ package Mohammad;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,19 +15,29 @@ import javax.servlet.http.HttpServletResponse;
  * A class can be made Servlet by just extending the class HttpServlet.
  * We are supposed to override the method service which is present in HttpServlet class.
 */
+
+
+//In this example we are going to see the demonstration of servlet annotations..
+	//Annotations are useful to avoid maitaining the xml file.
+	//It makes the devolopment easy as we dont have to set up xml file file and annottions will handle every thing..
+	//Given below is the demonstration of servlet annotations...
+
+@WebServlet("/add")
 public class Addition extends HttpServlet      
 {
  public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException 
  {
-     // This is a demonstration for ServletConfig
-	 
-	   
-	 PrintWriter out= res.getWriter();
-	 //Now to get the data which is set in web.xml we will use object of ServletConfig...
-	 ServletConfig cnt= getServletConfig();
-     out.println(cnt.getInitParameter("login-id")/*Here we are getting the parameter specified in web.xml*/);//And now we are printing the parmater...
-	 //Note: While ising ServletContext the context specified in web.xml file will be avialable for each and every servlet...
-	 
-  }
+	    long i= Integer.parseInt(req.getParameter("num1"));
+		long j=Integer.parseInt(req.getParameter("num2"));
+		  
+		  
+		
+		  Cookie cookie= new Cookie( "ans", (i+j)+ "" );
+	    res.addCookie(cookie); 	
+		
+	    res.sendRedirect("sq");
+	  
+  
+ }
 	
 }
--- src/Mohammad/Squre.java
+++ src/Mohammad/Squre.java
@@ -3,15 +3,38 @@ package Mohammad;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+//In this example we are going to see the demonstration of servlet annotations..
+	//Annotations are useful to avoid maitaining the xml file.
+	//It makes the devolopment easy as we dont have to set up xml file file and annottions will handle every thing..
+	//Given below is the demonstration of servlet annotations...
+
+@WebServlet("/sq")
 public class Squre extends HttpServlet
 {
 public void doGet( HttpServletRequest req, HttpServletResponse res) throws IOException
 {
+	Cookie cookies[]=req.getCookies();
+	long RD=0;
+	
+	for(Cookie cookie:cookies)
+	{
+		if(cookie.getName().equals("ans"))
+		{
+			RD=(long)Integer.parseInt(cookie.getValue());
+		}
+		
+	}
+	
+	PrintWriter out= res.getWriter();
+	out.println(RD);
+	out.println(RD*RD);
 	
 }
 
